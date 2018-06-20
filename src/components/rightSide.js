4c7c80032ef81dda5bbc364113dbf0305f4ca9ae
stevieschatz <stevieschatz91@gmail.com>
2018-06-20 17:04:09 +0200

    Added CV descriptions

--- src/components/rightSide.js
+++ src/components/rightSide.js
@@ -28,11 +28,6 @@ const RightSide = ({ siteTitle }) => (
       }}>Lieferando.de/Takeaway.com Berlin, Germany
     </h4>
 
-  {/* <h5 style={{
-    fontFamily: 'Didot',
-    fontWeight: 'bold',
-      }}>Berlin, Germany
-  </h5> */}
 
   <h6 style={{
     fontFamily: 'Didot',
@@ -42,13 +37,65 @@ const RightSide = ({ siteTitle }) => (
 
     <p style={{
       fontFamily: 'Didot',
-      }}>Designed and developed a dynamic Customer Service Portal for 11 different countries worldwide, the Customer Service Portal had a home grown backend system to allow our internal CS, Marketing, Press teams to add/update content for our end users.
+      }}>Designed and developed a dynamic Customer Service Portal for 11 different countries worldwide.
+    </p>
+
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Rebranded two new aquisitions In Bulgaria and Romania.
+    </p>
+
+  <h4 style={{
+    fontFamily: 'Didot',
+    fontWeight: 'bold',
+      }}>Health Dialog / Rite Aid Bedford, USA
+    </h4>
+
+
+  <h6 style={{
+    fontFamily: 'Didot',
+    fontWeight: 'bold',
+      }}>March 2017-September 2017
+  </h6>
+
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Lead and managed offshore and onsite teams to project specific deadlines.
+    </p>
+
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Implemented Oauth 2.0 and Single Sign On (SSO) in Interact (app_name).
+    </p>
+
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Executed the releases to Prod, QA, and Develop Environments.
     </p>
 
 
+    <h4 style={{
+    fontFamily: 'Didot',
+    fontWeight: 'bold',
+      }}>Lieferando.de/Takeaway.com Berlin, Germany
+    </h4>
 
 
+    <h6 style={{
+      fontFamily: 'Didot',
+      fontWeight: 'bold',
+        }}>March 2016-November 2016
+    </h6>
 
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Built microservices using Nodejs to meet business specific needs.
+    </p>
+
+    <p style={{
+      fontFamily: 'Didot',
+      }}>Collaborated with Backend, App, and API, teams to ensure consistent data with all applications throughout Europe.
+    </p>
 
   </div>
 </div>
