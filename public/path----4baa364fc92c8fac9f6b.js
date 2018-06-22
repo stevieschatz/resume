b6f5d98d79ffe794aa5cbe8292b81d24482c491e
stevieschatz <stevieschatz91@gmail.com>
2018-06-22 13:54:35 +0200

    remove untracked files

--- public/path----4baa364fc92c8fac9f6b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{103:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Stephen Schatzl"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----4baa364fc92c8fac9f6b.js.map
